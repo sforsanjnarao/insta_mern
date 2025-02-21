bb804da37f7d019750899f1db92230277f859d1e
sforsanjnarao <sanjnarao26@gmail.com>
2025-02-21 11:55:51 +0530

    first commit

---
+++ deploy/server.js
@@ -0,0 +1,9 @@
+const { Socket } = require('socket.io');
+const app=require('./src/app')
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', socket => {
+   console.log('User connected')
+});
+server.listen(3000,() => {console.log('app is on listen 3000')});
