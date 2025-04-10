313e001b2ffeccffd681057ec5420fa8f1026689
sforsanjnarao <sanjnarao26@gmail.com>
2025-04-11 00:41:17 +0530

    add user model, authentication routes, and utility functions for error handling

---
+++ refreshTokenRotation/server/users/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const authController = require("./controllers");
+const verifyJWT = require("../../middleware/verifyJWT");
+
+const router = express.Router();
+
+//Auth based routes
+router.post("/signUp", authController.SignUp);
+router.post("/signIn", authController.SignIn);
+router.get("/refresh", authController.refreshToken);
+router.get("/logout", authController.logOut);
+
+module.exports = router;
---
+++ refreshTokenRotation/server/utlis/TryCatch.js
@@ -0,0 +1,9 @@
+exports.tryCatch = (controller) => async (req, res, next) => {
+    try {
+      await controller(req, res);
+    } catch (error) {
+      console.log(error);
+      return next(error);
+    }
+  };
+  
